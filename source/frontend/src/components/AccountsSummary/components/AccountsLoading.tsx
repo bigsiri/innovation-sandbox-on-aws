bf05a092f86b1fbfcf499f95ad1e2dde73b76436
bigsiri <siriman0701@gmail.com>
2025-09-21 22:52:07 -0400

    i18n + french

--- source/frontend/src/components/AccountsSummary/components/AccountsLoading.tsx
+++ source/frontend/src/components/AccountsSummary/components/AccountsLoading.tsx
@@ -2,16 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Container } from "@cloudscape-design/components";
+import { useTranslation } from "react-i18next";
 
 import styles from "@amzn/innovation-sandbox-frontend/components/AccountsSummary/styles.module.scss";
 import { Loader } from "@amzn/innovation-sandbox-frontend/components/Loader";
 
 export const AccountsLoading = () => {
+  const { t } = useTranslation('accounts');
+  
   return (
     <Container>
       <div className={styles.container}>
         <div className={styles.middle}>
-          <Loader label="Loading account info..." />
+          <Loader label={t("loadingAccountInfo")} />
         </div>
       </div>
     </Container>
